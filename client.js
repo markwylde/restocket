ab52852dd7885a4ed550783975d1e8f7daff0dd9
Mark Wylde <me@markwylde.co.uk>
2018-07-29 01:59:13 +0100

    Add events and none RESP message parsing

--- client.js
+++ client.js
@@ -1,12 +1,19 @@
 import io from 'socket.io-client'
 import wildcard from 'socketio-wildcard'
+import EventEmitter from 'wolfy87-eventemitter'
+
 const patch = wildcard(io.Manager)
 
 export default class RestocketClient {
-  constructor () {
+  constructor (opts) {
     this.emitCount = 0
 
-    this.socket = io('http://localhost:3000', {
+    this._eventEmitter = new EventEmitter()
+
+    this.addEventListener = this._eventEmitter.addListener.bind(this._eventEmitter)
+    this.removeEventListener = this._eventEmitter.addListener.bind(this._eventEmitter)
+
+    this.socket = io(opts.host, {
       transports: ['websocket']
     })
 
@@ -16,12 +23,17 @@ export default class RestocketClient {
   waitForMessage (correlationId) {
     return new Promise((resolve, reject) => {
       const watcher = (message) => {
-        const headers = message.data[0][1]
-        const body = message.data[0][2]
-
-        if (headers._cid === correlationId) {
+        if (message.data[0][0] === 'RESP') {
+          const headers = message.data[0][1]
+          const body = message.data[0][2]
           this.socket.removeListener('*', watcher)
           resolve({body, headers})
+        } else {
+          const method = message.data[0][0]
+          const route = message.data[0][1]
+          const headers = message.data[0][2]
+          const body = message.data[0][3]
+          this._eventEmitter.emit('message', {method, route, body, headers})
         }
       }
       this.socket.on('*', watcher)
